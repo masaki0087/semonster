74f2231ef5f65ad0a9969b3ac41f446158bab4a4
masaki0087 <nabesan0087@gmail.com>
2026-06-22 10:10:58 +0900

    summon monster処理を実装

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
+    System.out.println("SEMonster!");
+    Monster m = new Monster();
+    System.out.println(m);
   }
 }
